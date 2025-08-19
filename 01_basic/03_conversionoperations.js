04c9d971b0980385bdc4b3cc0ccd1cc559504d11
Abhang Subhash Panchal <panchalabhang@gmail.com>
2025-08-19 11:27:05 +0000

    Conservation od datatypes

---
+++ 01_basic/03_conversionoperations.js
@@ -0,0 +1,24 @@
+let score = true
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//  "33" => 33
+//  "33abc" => NAN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let loggedIn = Boolean(isLoggedIn)
+console.log(loggedIn);
+
+// 1 => true ; 0=> false
+//  ""=> false
+// "Abhang" => true 
+
+let SomeNumber = String(33)
+console.log(typeof SomeNumber);
