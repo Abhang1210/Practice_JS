911a95cbaa3c12e5a08b9cebc89d6329e5cdd7be
Abhang Subhash Panchal <panchalabhang@gmail.com>
2025-08-19 10:13:38 +0000

    variable and datatypes done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "abhang@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState ;
+// accountId = 2 // not allowed
+console.log(accountId)
+
+accountEmail="abhangp@gmail.com"
+accountPassword= "286600"
+/*
+Please Do not Use Var 
+Beacause of the issue in block scope and functional scope 
+*/
+console.table([accountEmail,accountPassword, accountState]);
